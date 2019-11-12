887efe2fe39c2ab1990f02e191c7addc8847fec1
Austin Powell <apst12012@gmail.com>
2019-11-12 14:09:34 -0500

    Guided project

--- api/server.js
+++ api/server.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStorage = require('connect-session-knex')(session); // for storing sessions in DB
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const knexConnection = require('../database/dbConfig.js');
 
 const server = express();
 
+const sessionConfiguration = {
+  // default name is sid
+  name: 'lambda',
+  secret: process.env.COOKIE_SECRET || 'secret?',
+  cookie: {
+    maxAge: 1000 * 60 * 60, //valid for 1 hour (in milliseconds)
+    secure: process.env.NODE_ENV === 'development' ? false : true, // do we send cookie over https only?
+    httpOnly: true // prevent client Javascript code from accessing the cookie
+  },
+  resave: false, // save sessions even when they have not changed
+  saveUninitialized: true,
+  store: new KnexSessionStorage({
+    knex: knexConnection,
+    clearInterval: 1000 * 60 * 10, //delete expired sessions every 10 minutes
+    tablename: 'user_sessions',
+    sidfieldname: 'id',
+    createtable: true
+  })
+};
+
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use(cors()); // research 'creds: true' and 'withCredentials' when connecting from your react app
+server.use(session(sessionConfiguration));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+  res.json({ api: 'up', session: req.session });
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,6 +11,7 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
+      req.session.username = saved.username;
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -25,6 +26,7 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username; // Don't override the session object
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -35,6 +37,20 @@ router.post('/login', (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get('/logout', (req, res) => {
+  if( req.session) {
+    req.session.destroy(error => {
+      if(error) {
+        res.status(500).json({message: 'You cannot log out'})
+      } else {
+      res.status(200).json({message: 'logged out success'})
+      }
+    });
+  } else {
+    res.status(200).json({message: 'Already logged out'})
+  }
 });
 
 module.exports = router;
